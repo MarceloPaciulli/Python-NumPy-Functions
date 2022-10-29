0b82a12feedcb47221138803b7857a52b8b9d04f
Marcelo Paciulli <93230178+MarceloPaciulli@users.noreply.github.com>
2022-10-29 20:50:40 -0300

    Update methods-and-functions.py

--- methods-and-functions.py
+++ methods-and-functions.py
@@ -6,174 +6,175 @@ print(a)
 print(np.sort(a))
 print(np.unique(a))
 
-#-------------------------------------------------------------
 
+#one-hot encoding functions
 
-def get_targets_1(T):    
+def one_hot_encoding(T):
+    types = np.unique(T)
+    targets = np.eye(0).reshape(0,types.size)
+    for pos,item in enumerate(types):
+        a = np.insert(np.zeros((types.size-1)),pos,1)
+        block = np.tile(a,((np.array(T)==item).sum(),1))
+        targets=np.vstack((targets,block))
+    return targets
+
+#-------------------------------------------------------------------------------------------
+
+def one_hot_encoding2(T):    
     freq,lista = [],[]
-    rodar = np.array([1])[np.newaxis,:]
-    clases = np.unique(T)
-    rodar = np.delete(np.concatenate((rodar,np.zeros((1,clases.size))),axis=1),-1)
+    moving = np.array([1])[np.newaxis,:]
+    types = np.unique(T)
+    moving = np.delete(np.concatenate((moving,np.zeros((1,types.size))),axis=1),-1)
     e = list(T)
-    for elem in clases:
+    for elem in types:
         freq.append(e.count(elem))
     for pos, item in enumerate(freq):
         for k in range(item):
-            lista.append(np.roll(rodar,pos))
-    resultado = np.array(lista).reshape(len(T), clases.size)
-    return resultado         
+            lista.append(np.roll(moving,pos))
+    targets = np.array(lista).reshape(len(T), types.size)
+    return targets  
 
-#----------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 
-
-def get_targets_2(T):    
+def one_hot_encoding3(T):    
     freq,lista = [],[]
-    clases = np.unique(T)
-    rodar = np.eye(len(clases))
+    types = np.unique(T)
+    moving = np.eye(len(types))
     e = list(T)
-    for elem in clases:
+    for elem in types:
         freq.append(e.count(elem))
     for pos, item in enumerate(freq):
         for k in range(item):
-            lista.append(rodar[pos])
-    resultado = np.array(lista).reshape(len(T), clases.size)
-    return resultado          
+            lista.append(moving[pos])
+    targets = np.array(lista).reshape(len(T), types.size)
+    return targets     
 
-#-----------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 
-
-def get_targets_3(T):   
+def one_hot_encoding4(T):   
     freq = []
-    clases = np.unique(T)
-    t = np.zeros((len(T),clases.size))
-    rodar = np.delete(np.r_[np.array([1]),np.zeros((1,clases.size)).flatten()],-1)
-    for elem in clases:
+    types = np.unique(T)
+    targets = np.zeros((len(T),types.size))
+    moving = np.delete(np.r_[np.array([1]),np.zeros((1,types.size)).flatten()],-1)
+    for elem in types:
         freq.append(list(T).count(elem))
     index=0
     for pos, item in enumerate(freq):
-            t[index:index+item]=np.roll(rodar,pos)
+            targets[index:index+item]=np.roll(moving,pos)
             index+=item
-    return t                  
+    return targets    
 
-#----------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 
-
-def get_targets_4(T):    
-    clases = np.unique(T)
-    t = np.zeros((len(T),clases.size))
-    rodar = np.delete(np.hstack((np.eye(1),np.zeros((1,clases.size)))),-1)
-    freq={elem: list(T).count(elem) for elem in clases}
+def one_hot_encoding5(T):    
+    types = np.unique(T)
+    targets = np.zeros((len(T),types.size))
+    moving = np.delete(np.hstack((np.eye(1),np.zeros((1,types.size)))),-1)
+    freq={elem: list(T).count(elem) for elem in types}
     index=0
-    for pos, item in enumerate(clases):
-            t[index:index+freq[item]]=np.roll(rodar,pos)
+    for pos, item in enumerate(types):
+            targets[index:index+freq[item]]=np.roll(moving,pos)
             index+=freq[item]
-    return t                  
-#----------------------------------------------------------------
+    return targets      
 
-def get_targets_5(T):    
-    clases = np.unique(T)
-    t = np.zeros((len(T),clases.size))
-    rodar = np.delete(np.hstack((np.eye(1),np.zeros((1,clases.size)))),-1)
-    freq=list({elem: list(T).count(elem) for elem in clases}.values())
+#-------------------------------------------------------------------------------------------
+
+def one_hot_encoding6(T):    
+    types = np.unique(T)
+    targets = np.zeros((len(T),types.size))
+    moving = np.delete(np.hstack((np.eye(1),np.zeros((1,types.size)))),-1)
+    freq=list({elem: list(T).count(elem) for elem in types}.values())
     index=0
-    for pos in range(len(clases)):
-            t[index:index+freq[pos]]=np.roll(rodar,pos)
+    for pos in range(len(types)):
+            targets[index:index+freq[pos]]=np.roll(moving,pos)
             index=np.cumsum(freq)[pos]
-    return t                  
+    return targets    
 
-#------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 
-def get_targets_6(T):    
-    clases = np.unique(T)
-    t = np.zeros((len(T),clases.size))
-    rodar = np.delete(np.hstack((np.eye(1),np.zeros((1,clases.size)))),-1)
-    freq={elem: list(T).count(elem) for elem in clases}
+def one_hot_encoding7(T):    
+    types = np.unique(T)
+    targets = np.zeros((len(T),types.size))
+    moving = np.delete(np.hstack((np.eye(1),np.zeros((1,types.size)))),-1)
+    freq={elem: list(T).count(elem) for elem in types}
     index=0
-    for pos, item in enumerate(clases):
-            t[index:index+freq[item]]=np.roll(rodar,pos)
+    for pos, item in enumerate(types):
+            targets[index:index+freq[item]]=np.roll(moving,pos)
             index=np.cumsum(list(freq.values()))[pos]
-    return t                  
+    return targets
 
+#-------------------------------------------------------------------------------------------
 
-#----------------------------------------------------------------
+def one_hot_encoding8(T):
+    types = np.unique(T)
+    targets = np.array([]).reshape(0,types.size)
+    for pos,item in enumerate(types):
+        a = np.zeros_like(types,dtype=int)
+        a[pos]=1
+        block = np.tile(a,((np.array(T)==item).sum(),1))
+        targets=np.vstack((targets,block))
+    return targets
 
-def get_targets_7(T):
-	clases = np.unique(T)
-	target = np.array([]).reshape(0,clases.size)
-	for pos,item in enumerate(clases):
-		a = np.zeros_like(clases)
-		a[pos]=1
-		block = np.tile(a,((T==item).sum(),1))
-		target=np.vstack((target,block))
-	return target
-#-----------------------------------------------------------------        
+#-------------------------------------------------------------------------------------------
 
-def get_targets_8(T):
-	clases = np.unique(T)
-	target = np.eye(0).reshape(0,clases.size)
-	for pos,item in enumerate(clases):
-		a = np.insert(np.zeros((clases.size-1)),pos,1)
-		block = np.tile(a,((T==item).sum(),1))
-		target=np.vstack((target,block))
-	return target
-#------------------------------------------------------------------
+def one_hot_encoding9(T):
+    types = np.unique(T)
+    targets = np.eye(0).reshape(0,types.size)
+    for pos,item in enumerate(types):
+        block = np.tile(np.eye(len(types))[pos],((np.array(T)==item).sum(),1))
+        targets=np.vstack((targets,block))
+    return targets
 
-def get_targets_9(T):
-	clases = np.unique(T)
-	target = np.eye(0).reshape(0,clases.size)
-	for pos,item in enumerate(clases):
-		block = np.tile(np.eye(len(clases))[pos],((T==item).sum(),1))
-		target=np.vstack((target,block))
-	return target
+#-------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------
+def one_hot_encoding10(T):
+    init,types = 0,np.unique(T)
+    targets = np.zeros((len(T),types.size))
+    w = np.ones_like(targets)
+    for k,elem in enumerate(types):
+        freq = list(T).count(elem)
+        condlist = [w[init:init+freq]==1]
+        choicelist=[np.eye(len(types))[k]]
+        targets[init:init+freq]=np.select(condlist,choicelist)
+        init+=freq
+    return targets
 
-def get_targets_10(t):
-	init,clases = 0,np.unique(t)
-	t1 = np.zeros((len(t),clases.size))
-	w = np.ones_like(t1)
-	for k,elem in enumerate(clases):
-		cantidad = list(t).count(elem)
-		condlist = [w[init:init+cantidad]==1]
-		choicelist=[np.eye(len(clases))[k]]
-		t1[init:init+cantidad]=np.select(condlist,choicelist)
-		init+=cantidad
-	return t1
-#-----------------------------------------------------------------
-            
-def get_targets_11(t):                                          
-	init = 0
-	clases = np.unique(t)
-	fila = np.delete(np.insert(np.zeros_like(clases),0,1),-1)
-	target = np.eye(0).reshape(0,clases.size)
-	w = np.ones((len(t),clases.size))
-	for k,elem in enumerate(clases):
-		cantidad = list(t).count(elem)
-		condlist = [w[init:init+cantidad]==1]
-		choicelist=[np.roll(fila,k)]
-		target = np.vstack((target,np.select(condlist,choicelist)))
-		init+=cantidad
-	return target
+#-------------------------------------------------------------------------------------------
 
-#------------------------------------------------------------------
+def one_hot_encoding11(T):                                          
+    init = 0
+    types = np.unique(T)
+    row = np.delete(np.insert(np.zeros_like(types,dtype=int),0,1),-1)
+    targets = np.eye(0).reshape(0,types.size)
+    array_ones = np.ones((len(T),types.size))
+    for k,elem in enumerate(types):
+        freq = list(T).count(elem)
+        condlist = [array_ones[init:init+freq]==1]
+        choicelist=[np.roll(row,k)]
+        targets = np.vstack((targets,np.select(condlist,choicelist)))
+        init+=freq
+    return targets
 
-def get_targets_12(t):                               
-	init = 0
-	clases = np.unique(t)
-	t1 = np.zeros((len(t),clases.size))
-	fila = np.delete(np.insert(np.zeros_like(clases),0,1),-1)
-	w = np.ones((len(t),clases.size))
-	for k,elem in enumerate(clases):
-		cantidad = list(t).count(elem)
-		condlist = [w[init:init+cantidad]==1]
-		choicelist=[np.roll(fila,k)]
-		t1[init:init+cantidad]=np.select(condlist,choicelist)
-		init+=cantidad
-	return t1
+#-------------------------------------------------------------------------------------------
+
+def one_hot_encoding12(T):                               
+    init = 0
+    types = np.unique(T)
+    targets = np.zeros((len(T),types.size))
+    row = np.delete(np.insert(np.zeros_like(types,dtype=int),0,1),-1)
+    array_ones = np.ones((len(T),types.size))
+    for k,elem in enumerate(types):
+        freq = list(T).count(elem)
+        condlist = [array_ones[init:init+freq]==1]
+        choicelist=[np.roll(row,k)]
+        targets[init:init+freq]=np.select(condlist,choicelist)
+        init+=freq
+    return targets
+
 
 
 #let's try!
 
-print(get_targets_1(a))
+print(one_hot_encoding(categories).astype(int))
 
 
